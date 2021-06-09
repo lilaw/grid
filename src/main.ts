e19be37af43f2190bf60f0a5c3458c90ea62f2ec
Your Name <takialog@gmail.com>
2021-06-09 16:45:43 +0800

    change type in store

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { store, key } from './store'
+import { store } from "./store";
 
-createApp(App).use(store, key).mount("#app");
+createApp(App).use(store).mount("#app");
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,4 @@
-import { InjectionKey } from "vue";
-import { createStore, useStore as baseUseStore, Store } from "vuex";
+import { createStore, Store as VuexStore } from "vuex";
 
 type unit = { unit: string };
 // type place = {
@@ -16,7 +15,43 @@ export type State = {
   childarea: string[];
 };
 
-export const key: InjectionKey<Store<State>> = Symbol();
+
+
+export type Mutations<S = State> = {
+  updateColumns(state: S, payload: number): void,
+  updateRows(state: S, payload: number): void,
+  updateColumnGap(state: S, payload: string): void,
+  updateRowGap(state: S, payload: string): void
+  adjustArr(state: S, payload: { newv: number; oldv: number; direction: "columnArr" | "rowArr" }): void,
+  addChildarea(state: S, payload: string): void,
+  removeChildarea(state: S, payload: number): void,
+}
+
+export type Getters<S = State> = {
+  divCount(state: S): number,
+  templateCol(state: S): string,
+  templateRow(state: S): string,
+}
+
+
+export type Store = Omit<
+  VuexStore<State>,
+  'getters' | 'commit' | 'dispatch'
+> & {
+  commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(
+    key: K,
+    payload: P 
+  ): ReturnType<Mutations[K]>
+}& {
+  getters: {
+    [K in keyof Getters]: ReturnType<Getters[K]>
+  }
+}
+
+
+
+
+
 
 export const store = createStore<State>({
   state: {
@@ -29,21 +64,21 @@ export const store = createStore<State>({
     childarea: [],
   },
   mutations: {
-    updateColumns(state, payload: number) {
+    updateColumns(state: State, payload: number) {
       state.columns = payload;
     },
-    updateRows(state, payload: number) {
+    updateRows(state: State, payload: number) {
       state.rows = payload;
     },
-    updateColumnGap(state, payload: string) {
+    updateColumnGap(state: State, payload: string) {
       state.columnGap = payload;
     },
-    updateRowGap(state, payload: string) {
+    updateRowGap(state: State, payload: string) {
       state.rowGap = payload;
     },
     adjustArr(
-      state,
-      payload: { newv: number; oldv: number; direction: "columnArr" | "rowArr" }
+      state: State,
+      payload: { newv: number; oldv: number; direction: "columnArr" | "rowArr" } 
     ) {
       const difference = Math.abs(payload.newv - payload.oldv);
 
@@ -56,22 +91,22 @@ export const store = createStore<State>({
         state[payload.direction].splice(payload.newv - 1, difference);
       }
     },
-    addChildarea(state, payload: string) {
+    addChildarea(state: State, payload: string) {
       state.childarea.push(payload);
     },
-    removeChildarea(state, payload: number) {
+    removeChildarea(state: State, payload: number) {
       console.log(payload)
       state.childarea.splice(payload, 1);
     },
   },
   getters: {
-    divCount(state) {
+    divCount(state: State) {
       return state.columns * state.rows;
     },
-    templateCol(state) {
+    templateCol(state: State) {
       return createRepeat(groupRepeat(state.columnArr));
     },
-    templateRow(state) {
+    templateRow(state: State) {
       return createRepeat(groupRepeat(state.rowArr));
     },
   },
@@ -79,12 +114,9 @@ export const store = createStore<State>({
   modules: {},
 });
 
-// define your own `useStore` composition function
-export function useStore(): typeof store {
-  return baseUseStore(key);
-}
 
-function groupRepeat(template: unit[]) {
+
+function groupRepeat(template: unit[]): string[][] {
   return template
     .map((it) => it.unit)
     .reduce((acc: string[][], cur) => {
@@ -99,8 +131,12 @@ function groupRepeat(template: unit[]) {
     }, []);
 }
 
-function createRepeat(group: string[][]) {
+function createRepeat(group: string[][]): string {
   return group
     .flatMap((g) => (g.length > 1 ? `repeat(${g.length}, ${g[0]})` : g))
     .join(" ");
+}
+
+export function useStore() {
+  return store as Store
 }
